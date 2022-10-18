271e157a02127badd2ca5b7da0931df25fec5744
Steven Khoi Xuan Vo <116126440+StevenVo5853@users.noreply.github.com>
2022-10-18 16:43:54 -0700

    Update board.h

--- board.h
+++ board.h
@@ -1,16 +1,8 @@
-/**
- * Assignment: life
- * Name:TODO
- * PID :TODO
- * Class: UCSD CSE30-sp20
- *
- */
+
 #ifndef _BOARD_H
 #define _BOARD_H
-#include "cse30life.h"
+#include "cselife.h"
 
-// we can change the board type to different sizes to see how this affects the speed.
-// DO NOT CHANGE - libcse30life library assumes unsigned char
 typedef unsigned char belem;
 
 
@@ -20,12 +12,12 @@ struct boards {
 	belem *bufferB;			// pointer to ansecond life board buffer
 	uint32_t numRows;		// number of rows in the life board
 	uint32_t numCols;		// number of cols in the life board
-	belem *currentBuffer;	// pointer to the current life board's buffer 
+	belem *currentBuffer;	        // pointer to the current life board's buffer 
 	belem *nextBuffer;		// pointer to the next iteration's board buffer
 	uint32_t gen;			// generation number   
 };
 
-extern boards_t *createBoard(char *filename);		// create a board structure
+extern boards_t *createBoard(char *filename);		        // create a board structure
 extern void deleteBoard(boards_t **);				// delete a board structure
 extern void clearBoards(boards_t *self);			// clear the boards (current and next)
 extern void swapBuffers(boards_t *self);			// swap the board current and next
